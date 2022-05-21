d47ea7d53666f82c3b77ef4102e6579da28ea5a7
Niima1985 <67212610+Niima1985@users.noreply.github.com>
2022-05-21 16:37:10 -0700

    Update vehicle.js

--- vehicle.js
+++ vehicle.js
@@ -69,7 +69,7 @@ class Vehicle {
 
     autoDrive()
     {
-      
+    
     }
 
     typeOfVehicle(wheels) {
@@ -86,6 +86,4 @@ class Vehicle {
 }
 
 //This exports things you want to use from this "module", more info in readme
-module.exports = {
-    Vehicle
-}
+module.exports = { Vehicle }
